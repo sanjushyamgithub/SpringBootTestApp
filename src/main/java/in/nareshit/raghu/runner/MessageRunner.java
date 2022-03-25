18e90e9e1375ecef11c1eb3749ad0427b2db7fbd
Sanjeev <kumarsanjugt@gmail.com>
2022-03-25 11:33:18 -0700

    MH-8520- SECOND TIME UPDATED

--- src/main/java/in/nareshit/raghu/runner/MessageRunner.java
+++ src/main/java/in/nareshit/raghu/runner/MessageRunner.java
@@ -8,8 +8,9 @@ public class MessageRunner implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		// TODO Auto-generated method stub
+		
 		System.out.println("WELCOME TO SPRING BOOT APP!");
+		
 
 	}
 
