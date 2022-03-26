26dba61c4a4c10ddf1a686c2e0f72ba531f5b8d1
Sanjeev <kumarsanjugt@gmail.com>
2022-03-25 23:04:28 -0700

    WH-4921- UPDATE CODE WITH LOG4J

--- src/main/java/in/nareshit/raghu/runner/MessageRunner.java
+++ src/main/java/in/nareshit/raghu/runner/MessageRunner.java
@@ -1,17 +1,27 @@
 package in.nareshit.raghu.runner;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MessageRunner implements CommandLineRunner {
 
+	private static Logger log=LoggerFactory.getLogger(MessageRunner.class);
 	@Override
 	public void run(String... args) throws Exception {
 		
 		int a=0;
 		int b=0;
-		System.out.println("HELLO FILE UPDATED!" +(a+b));
+		try {
+			log.info("Started!");
+			System.out.println("HELLO FILE UPDATED!" +(a+b));
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		log.info("End");
+		
 		
 
 	}
