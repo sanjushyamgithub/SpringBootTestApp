7e4f9aa72c8554c8380899ff57363862445261b2
Sanjeev <kumarsanjugt@gmail.com>
2022-03-25 22:52:21 -0700

    WH-9292 -COMMIT THIRD TIME

--- src/main/java/in/nareshit/raghu/runner/MessageRunner.java
+++ src/main/java/in/nareshit/raghu/runner/MessageRunner.java
@@ -10,7 +10,8 @@ public class MessageRunner implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		
 		int a=0;
-		System.out.println("HELLO FILE UPDATED!");
+		int b=0;
+		System.out.println("HELLO FILE UPDATED!" +(a+b));
 		
 
 	}
